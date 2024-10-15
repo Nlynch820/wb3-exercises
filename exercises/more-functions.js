f9528f48e99a40d46531f2f726194bc5666c7210
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-15 14:10:40 -0400

    more functions

---
+++ exercises/more-functions.js
@@ -0,0 +1,13 @@
+"use strict"
+
+//function displayMailingLabel(name, address, city, state, zip){
+   // console.log(name, address, city, state, zip);
+    
+//}
+//displayMailingLabel("Nick", "123 street", "Bronx", "NY", "11002")
+
+// function addNumbers(num1, num2) {
+//     console.log(num1 + "+" + num2 + "=" + (num1 + num2));
+    
+// }
+// addNumbers(1, 4)
