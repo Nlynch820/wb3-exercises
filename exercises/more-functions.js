8e5560d3fd27dd45fe78f28c97e1c0417665cacb
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-15 14:42:02 -0400

    fun with functions

--- exercises/more-functions.js
+++ exercises/more-functions.js
@@ -1,16 +1,18 @@
 "use strict"
 
-//function displayMailingLabel(name, address, city, state, zip){
-   // console.log(name, address, city, state, zip);
+function displayMailingLabel(name, address, city, state, zip){
+   console.log(name, address, city, state, zip);
     
-//}
-//displayMailingLabel("Nick", "123 street", "Bronx", "NY", "11002")
+}
+displayMailingLabel("Nick", "123 street", "Bronx", "NY", "11002")
+displayMailingLabel("John Doe", "1600 Pennsylvania Ave" ,"Washington", "DC", "20500")
 
-// function addNumbers(num1, num2) {
-//     console.log(num1 + "+" + num2 + "=" + (num1 + num2));
+function addNumbers(num1, num2) {
+    console.log(num1 + "+" + num2 + "=" + (num1 + num2));
     
-// }
-// addNumbers(1, 4)
+}
+addNumbers(1, 4)
+addNumbers(918, 3322)
 
 function displayReceipt(totalDue, amountPaid) {
     console.log("Total Due: $" + totalDue);
@@ -19,3 +21,5 @@ function displayReceipt(totalDue, amountPaid) {
     
 }
 displayReceipt(57.66, 100)
+displayReceipt(50,50)
+displayReceipt(85, 75)
