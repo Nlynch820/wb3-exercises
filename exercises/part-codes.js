0b0f5873f719ffe74ac06dea6be275d78eee9499
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-17 10:36:03 -0400

    exercises complete

--- exercises/part-codes.js
+++ exercises/part-codes.js
@@ -1,25 +1,56 @@
 "use strict";
 
+// my way
+// function getSupplier(code) {
+//     const index = code.indexOf(":");
+//     const previousCharacters = code.substring(0, index)
+//     console.log(previousCharacters);
+
+// }
+
+// function getProductNumber(code) {
+//   const index = code.indexOf(":");
+//   const secondIndex = code.indexOf("-");
+//   const productNumber = code.substring(index + 1, secondIndex);
+//   console.log(productNumber);
+// }
+
+// function getSize(code) {
+//   const index = code.indexOf("-");
+//   const size = code.substring(index + 1);
+//   console.log(size);
+// }
+
+// getSupplier("ACME:123-L");
+// getProductNumber("DI:12345-M");
+// getSize("ACE:1-12");
+
+// craigs way
+const code1 = "ACME:123-L";
+const code2 = "DI:12345-M";
+const code3 = "ACE:1-12";
+
 function getSupplier(code) {
-    const index = code.indexOf(":");
-    const previousCharacters = code.substring(0, index)
-    console.log(previousCharacters);
-    
+  let positionOfColon = code.indexOf(":");
+  let supplier = code.substring(0, positionOfColon);
+  return supplier;
 }
+// let supplier = getSupplier(code1);
+console.log(getSupplier(code1), getSupplier(code2), getSupplier(code3));
 
 function getProductNumber(code) {
-  const index = code.indexOf(":");
-  const secondIndex = code.indexOf("-");
-  const productNumber = code.substring(index + 1, secondIndex);
-  console.log(productNumber);
+  let positionOfColon = code.indexOf(":");
+  let positionOfHyphen = code.lastIndexOf("-");
+  let productNumber = code.substring(positionOfColon + 1, positionOfHyphen);
+  return productNumber;
 }
+// let productNumber = getProductNumber(code2);
+console.log(getProductNumber(code1), getProductNumber(code2), getProductNumber(code3));
 
 function getSize(code) {
-  const index = code.indexOf("-");
-  const size = code.substring(index + 1);
-  console.log(size);
+  let positionOfHyphen = code.lastIndexOf("-");
+  let size = code.substring(positionOfHyphen + 1);
+  return size;
 }
-
-getSupplier("ACME:123-L");
-getProductNumber("DI:12345-M");
-getSize("ACE:1-12");
+// let size = getSize(code3);
+console.log(getSize(code1), getSize(code2), getSize(code3));
