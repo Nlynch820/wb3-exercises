9e8b8d36a465509ac42cf2001d52b214c8765578
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-17 09:33:15 -0400

    function returns

---
+++ exercises/function-returns.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// function greet() {
+//     let greet = "Hello World";
+//     return greet;
+// }
+// console.log(greet);
+
+
+// function addNumbers(num1, num2){
+//     let sum = num1 + num2;
+//     return sum; 
+// }
+// let answer = addNumbers(10,5)
+// console.log(answer);
+
+
+// function isEven(num){
+//     if (num % 2 == 0){
+//         return "Even"
+//     } else{
+//         return "Odd"
+//     }
+// }
+// console.log(isEven(4));
+// console.log(isEven(9));
+
