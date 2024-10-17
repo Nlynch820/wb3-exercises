2ba022edd3abd8b09b9963fb497b7fff86ece9c6
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-17 13:59:09 -0400

    website w html and js

---
+++ website1/scripts/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let mainParagraph = document.getElementById("mainParagraph");
+mainParagraph.style.fontStyle = "italic";
+mainParagraph.style.backgroundColor = "cyan"
+console.log(mainParagraph);
